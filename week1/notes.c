e90a550e285ce29913a7f7d5f914648f28f2de9a
druvesque <dhruvsachdeva6480@gmail.com>
2026-02-04 22:31:27 +0530

    w1

---
+++ week1/notes.c
@@ -0,0 +1,6 @@
+// MOTIVATION OF THE COURSE
+//
+// - There has been a computing paradigm shift from logic-centric computing
+//   to memory-centric computing. 
+//
+// - However, computing infrastructure has not changed fundamentally.
